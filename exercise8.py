29c1c5f4d03dc31acff6fbc5c72f417d8a7a100e
Nick Benos <nbenos@enova.com>
2017-06-21 16:42:04 -0500

    working exercise8

---
+++ exercise8.py
@@ -0,0 +1,13 @@
+import random
+
+#player = input("Let's play rock, paper scissor. Please choose one in 3..2..1: ")
+
+rock = 1
+paper = 2
+scissors = 3
+
+computer_list = [rock, paper, scissors]
+
+random_comp_list = []
+
+
