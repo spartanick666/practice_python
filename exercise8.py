903844cbfcf218b4aaa6a15598add78c2bd7306d
Nick Benos <nbenos@enova.com>
2017-06-22 13:31:03 -0500

    modified exercise8 with less lines

--- exercise8.py
+++ exercise8.py
@@ -3,9 +3,11 @@ player2 = input("Player2, Please pick rock, paper, or scissors: ")
 
 if player1 == player2:
     print("The game is a tie")
-elif player1 == ("rock") and player2 == ("paper") or player1 == ("paper") and player2 == ("scissors") or player1 ==("scissors") and player2 == ("rock"):
+elif player1 == ("rock") and player2 == ("paper") or player1 == ("paper") and player2 == ("scissors") \
+        or player1 ==("scissors") and player2 == ("rock"):
     print("Player 2 Wins!")
-elif player2 == ("rock") and player1 == ("paper") or player2 == ("paper") and player1 == ("scissors") or player2 ==("scissors") and player1 == ("rock"):
+elif player2 == ("rock") and player1 == ("paper") or player2 == ("paper") and player1 == ("scissors") \
+        or player2 ==("scissors") and player1 == ("rock"):
     print("Player 1 Wins!")
 else:
     print("Did you choose correctly?")
