4ffc205a79df8277ff5546e5db5d961301a389db
Nick Benos <nbenos@enova.com>
2017-06-25 09:34:17 -0500

    final exercise12.py

--- exercise12.py
+++ exercise12.py
@@ -1,2 +1,10 @@
+import random
 
+random_list = random.sample(range(100), 15)
+
+def first_last():
+    return(random_list[0], random_list[-1])
+
+print("Your random list is: ", random_list)
+print("\nThis first and last elements in the list are: ", first_last())
 
