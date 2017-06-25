7a283f80129bf64876a59f4bf61cc17502e11c03
Nick Benos <nbenos@enova.com>
2017-06-25 08:54:49 -0500

    final exercise11 and blank exercise12

--- exercise11.py
+++ exercise11.py
@@ -1,3 +1,17 @@
-quit = input('type "enter" to quit: ')
-while quit != "enter":
-    quit = input('type "enter" to quit: ')
+def get_number():
+    return int(input("Please enter a number: "))
+
+def not_prime():
+    if number % i == 0:
+        print("This is not prime number")
+
+def is_prime():
+    if number % i != 0:
+        print("This is a prime number")
+
+number = get_number()
+
+for i in range(2, number):
+   not_prime()
+   is_prime()
+   break
