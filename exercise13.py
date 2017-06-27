28ebd397edb6e7bfaaed6df06a532f1b6323a476
Nick Benos <nbenos@enova.com>
2017-06-27 16:25:14 -0500

    adding exercise13

---
+++ exercise13.py
@@ -0,0 +1,23 @@
+#Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
+#Take this opportunity to think about how you can use functions. Make sure to ask the user to enter the number of
+#numbers in the sequence to generate.(Hint: The Fibonnaci seqence is a sequence of numbers where the next number in
+#the sequence is the sum of the previous two numbers in the sequence.
+#The sequence looks like this: 1, 1, 2, 3, 5, 8, 13)
+
+fib_num = int(input("How many Fibonnaci numbers should I generate? Please enter a number: "))
+fib_list = []
+
+for i in range(0, fib_num + 1):
+    fib_list.append(i)
+#print(fib_list)
+
+def fib(n):
+   if n == 1:
+      return 1
+   elif n == 0:
+      return 0
+   else:
+    return fib(n-1) + fib(n-2)
+
+for x in fib_list:
+    print(fib(x))
