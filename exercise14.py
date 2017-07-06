5ed62a5b9faa87641ec9d9c74cf6d8e84a0624ea
Nick Benos <nbenos@enova.com>
2017-07-06 14:18:59 -0500

    add final ex14

--- exercise14.py
+++ exercise14.py
@@ -1,9 +1,11 @@
-repeat_list = [1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 7, 7, 8, 9, 10, 10, 10, 10]
+repeat_list = [0, 0, 1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 7, 7, 8, 9, 10, 10, 10, 10, 11, 11, 11, 11, 11]
 norepeat_list = []
 
-
-#def remove_dups():
-for x in repeat_list:
-        if x == True:
+def remove_dups():
+    for x in repeat_list:
+        while x > len(repeat_list):
+            #why does this next line of code needed to work?
             norepeat_list.append(x)
-print(norepeat_list)
+    return set(repeat_list)
+
+print(remove_dups())
