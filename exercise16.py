522465a1f9ea6172b1ed5ce124cc8ae24a82477a
Nick Benos <nbenos@enova.com>
2017-07-10 16:37:53 -0500

    add ex16

--- exercise16.py
+++ exercise16.py
@@ -1,7 +1,16 @@
-ll = [1,2,3,4,5]
-txt = ''
-for item in ll:
-    txt += str(item)
+import random
 
-print(txt)
+def random_num():
+    return random.randint(0, 100)
+
+def random_low():
+    return random.choice('abcdefghijklmnopqrstuvwxyz')
+
+def random_cap():
+    return random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+def random_sym():
+    return random.choice('~!@#$%^&*()_+[];,./{}:"<>?')
+
+pwd_list = [(random_num(),random_sym(),random_cap(),random_num(),random_low())]
 
