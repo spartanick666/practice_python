577dc2223f35130cecb2cf3e4653331475487bf8
Nick Benos <nbenos@enova.com>
2017-07-11 15:29:36 -0500

    mod ex16

--- exercise16.py
+++ exercise16.py
@@ -1,7 +1,7 @@
 import random
 
 def random_num():
-    return random.randint(0, 100)
+    return random.randint(0, 100).__str__()
 
 def random_low():
     return random.choice('abcdefghijklmnopqrstuvwxyz')
@@ -12,5 +12,16 @@ def random_cap():
 def random_sym():
     return random.choice('~!@#$%^&*()_+[];,./{}:"<>?')
 
-pwd_list = [(random_num(),random_sym(),random_cap(),random_num(),random_low())]
+pwd_list = [random_num(), random_sym(), random_cap(), random_num(), random_low(), random_sym()]
 
+def random_pwd():
+    return ''.join(pwd_list)
+
+user_input = input("Hello, would you like to generate a random password? please select yes or no: ").lower()
+
+if user_input == "yes":
+    print(random_pwd())
+elif user_input == "no":
+    print("Don't you want a password?")
+else:
+    print("Sorry I don't understand")
