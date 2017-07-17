650997fe3d1787a85e581e9c8fe1da0f0aaa905e
Nick Benos <nbenos@enova.com>
2017-07-17 09:11:10 -0500

    mod ex16

--- exercise16.py
+++ exercise16.py
@@ -1,4 +1,4 @@
-#Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of
+# Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of
 # lowercase letters, uppercase letters, numbers, and symbols. The passwords should be random, generating a new password
 # every time the user asks for a new password. Include your run-time code in a main method.
 
@@ -23,7 +23,7 @@ def weak_pwd():
     weak_list = ['love', '12345', 'password', 'guest', 'qwerty', 'root']
     return ("Here is your weak password: " + random.choice(weak_list))
 
-pwd_list = [random_num(), random_sym(), random_cap(), random_num(), random_low(), random_sym()]
+pwd_list = [random_num(), random_sym(), random_cap(), random_low(), random_sym(), random_low(), random_cap()]
 
 def random_pwd():
     return ''.join(pwd_list)
