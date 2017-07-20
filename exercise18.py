040f4b251651edd849ec3c36d117946a8bb5bb4d
spartanick666 <nickbenos@gmail.com>
2017-07-20 16:40:35 -0500

    mod ex18

--- exercise18.py
+++ exercise18.py
@@ -1,14 +1,22 @@
-#Use the BeautifulSoup and requests Python packages to print out a list of all the article titles
-# on the New York Times homepage.
+#Create a program that will play the “cows and bulls” game with the user. The game works like this: Randomly generate a
+#4-digit number. Ask the user to guess a 4-digit number. For every digit that the user guessed correctly in the correct
+#place, they have a “cow”. For every digit the user guessed correctly in the wrong place is a “bull.” Every time the user
+#makes a guess, tell them how many “cows” and “bulls” they have. Once the user guesses the correct number, the game is
+#over. Keep track of the number of guesses the user makes throughout teh game and tell the user at the end.
 
-import requests
+import random
 
-url = 'https://www.nytimes.com/'
-r = requests.get(url)
-r_html = r.text
+rand_num = str(random.randint(1000, 9999))
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(r_html, 'html.parser')
+rand_list = []
 
-print(soup(class_="story-heading"))
+for x in rand_num:
+    rand_list.append(x)
+print(rand_list)
 
+number = input("Welcome to the Cows and Bulls game, Please enter in a 4 digit number: ")
+
+num_list = []
+for x in number:
+    num_list.append(x)
+print(num_list)
